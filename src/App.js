ba941e6f37346df7cdbacb0c20c4745e32f04654
Yukti Jhawar <yuktimj2002@gmail.com>
2023-07-16 15:17:35 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <h1 className="font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Navbar/>
+    </>
   );
 }
 
