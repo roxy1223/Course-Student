b177b6a404fca8e94e81069921caa269004a7904
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-07-17 17:08:29 +0530

    page20-benifits-of-purchase

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Page57 from './Pages/Page57';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Page19 from './Pages/Page19';
+import Page20 from './Pages/Page20';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/page57' element={<Page57/>} />
           <Route path='/page19' element={<Page19/>} />
+          <Route path='/page20' element={<Page20/>} />
         </Routes>
       </BrowserRouter>
     </>
