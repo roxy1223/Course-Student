d0f665d525ee192f6d1f68c3e4cd5f89484c24eb
roxy1223 <shalinisharma1531@gmail.com>
2023-08-03 10:26:59 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -4,19 +4,15 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 import Login from "./Pages/Login/index";
 import Signup from "./Pages/Signup/index";
-import Navbar from "./Components/Navbar";
-import Footer from "./Components/Footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Navbar/>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
-        <Footer/>
       </BrowserRouter>
     </>
   );
